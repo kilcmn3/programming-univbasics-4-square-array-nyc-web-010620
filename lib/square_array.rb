2f54a4cfba904812183e9cbf8b70046483be45a2
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-28 02:39:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   count = 0;
   while count < array.length do
-    newarray.push(array[count] * array[count])
+    new_array.push(array[count] * array[count])
     count += 1
 
   end
